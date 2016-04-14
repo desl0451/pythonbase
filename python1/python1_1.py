cfcfda180957f1c58f44c65ac764c884aa89c2a1
王涛 <desl0451@github.com>
2016-04-14 20:58:41 +0800

    Initial commit

---
+++ python1/python1_1.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+#  注释
+
+# 打印圆的周长
+pi = 3
+radius = 5
+print 2 * pi * radius
+
+# 获得用户名
+# user_name = raw_input("What is your name?");
+# print user_name
+
+print "Hello 'hello'"
+
+# 拼接字符串
+print 'hello' 'world'
+
+print 'haha' + 'hehe'
+
+# str函数
+print repr('Hello,world!')  # 'Hello,world!'
+
+print str(10000L)  # 10000
+
+# input 和raw_input的比较
+# raw data原始数据
+# print input("Enter a number:")
+#
+# print raw_input("Enter a number:")
+
+# 原始字符串
+print 'hello.\nworld!'
+
+# 路径 c:\
+print 'c:\\na'
+print r'c:\na' #前面加r后变为路径显示 c:\na
+
+#函数总结
+# abs绝对值
+# float字符串转符点数
+# input获取用户输入
+# int 字符串转int
+# long 字符串转长整型
+# repr(object) 返回值的字符串形式
---
+++ python2/python2_1.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+# python包含6种内建的序列
+
+raw
